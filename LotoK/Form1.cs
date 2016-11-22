e629fc005cc65d1a6f8b9512eecb8a8ca37e4fd5
kPlusPlus <kresimir.ivkovic@gmail.com>
2016-11-22 23:05:53 +0100

    modified:   LotoK/Form1.cs 	modified:   LotoK/LotoK.csproj 	new file:   LotoK/packages.config

--- LotoK/Form1.cs
+++ LotoK/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace LotoK
 {
@@ -24,7 +26,31 @@ namespace LotoK
             LO = new Loto();
             LO.Generate();
 
+            txtCombination.Text = RetLotoComb();
+            SaveJSON();
+
             MessageBox.Show("aaa " + LO.Numbers[0] + " " + LO.Numbers[4] );
         }
+
+        private string RetLotoComb() {
+            string retVal = string.Empty;
+            foreach( var item in LO.Numbers )
+            {
+                retVal += item.ToString() + " | ";
+            }
+            foreach( var item in LO.ReserveNumbers )
+            {
+                retVal += item.ToString() + " | ";
+            }
+
+            return retVal;
+        }
+
+        private void SaveJSON()
+        {
+            string sJson = JsonConvert.SerializeObject(LO);
+            File.WriteAllText(txtFileName.Text, sJson);
+        }
+
     }
 }
